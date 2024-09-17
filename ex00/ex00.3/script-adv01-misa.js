c66e45723c3bce13717f17c1fa4397cb91f168af
Misasun1 <misa1996@yahoo.com>
2024-09-17 18:49:12 +0200

    ex1-adv-done

---
+++ ex00/ex00.3/script-adv01-misa.js
@@ -0,0 +1,28 @@
+
+// first statement--> declairing variables
+
+let a = 7;
+let b = 14;
+let c = 21;
+let d = 36;
+let e = 42;
+
+//second statement --> sum all the numbers
+
+let sum =[a+b+c+d+e];
+console.log(sum);
+
+//third statement--> declairing new values
+let f = 1;
+let g = 15;
+let h = 8;
+let i = 1;
+
+// fourth statement--> multiply 
+let multiply =[ f*g*h*i];
+console.log(multiply);
+
+// fifth statement --> dividing sum and multiply
+document.write(sum/multiply)
+
+
