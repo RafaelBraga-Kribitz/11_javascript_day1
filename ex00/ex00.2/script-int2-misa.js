9000c515c462a20b25f2c472f2808648c56eb730
Misasun1 <misa1996@yahoo.com>
2024-09-17 17:32:15 +0200

    int-ex2-misa-done

---
+++ ex00/ex00.2/script-int2-misa.js
@@ -0,0 +1,50 @@
+//first statement
+ let fruits =['apple','banana','kiwi', 'mango', 'pear','orange'];
+ console.log(fruits);
+
+//  second statement
+fruits.shift();
+console.log(fruits);
+
+// third statement
+fruits.unshift('apple');
+fruits.splice(5,6);
+console.log(fruits);
+
+// forth statement
+fruits.shift();
+fruits.unshift('strawberry');
+console.log(fruits);
+
+// ////////animals array/////////////
+
+// first statement
+let animals =['horse','dog','elephant','giraffe'];
+console.log(animals);
+
+// second statement
+animals.unshift('cat');
+console.log(animals);
+
+// third statement
+animals.pop();
+console.log(animals);
+
+// fourth statement
+animals.splice(4,0,"tiger");
+console.log(animals);
+
+// fifth statement
+animals.sort();
+animals.reverse();
+console.log(animals);
+
+
+
+
+
+
+
+
+
+
